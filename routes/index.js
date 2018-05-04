1524c1d3b7279b0af9301bb86879a11e28691e06
leonadia <leonadia1121@gmail.com>
2018-05-04 14:38:02 -0700

    with mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
+bodyParser = require('body-parser')
+var todo = require('../controller/todo_controller');
+var ejs = require('ejs')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+app.set('view engine', 'ejs');
+//app.use(express.static('public'));
+app.get('/index', function (req, res) {
+    res.render("C:/PROJECTS/NodeJs/DEMOAPP/todoapp/views/index.ejs")
+})
 
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   todo_req = {
+    title:req.query.title,
+    description:req.query.description,
+    status:req.query.status
+ };
+  try {
+    var createTODO = todo.createTodo(todo_req,response);
+    response = {
+      status: 201,
+      response: createTODO
+    };
+  } 
+  catch(e)
+  {
+    response = "not succes";
+  }
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+
+})
 module.exports = router;
